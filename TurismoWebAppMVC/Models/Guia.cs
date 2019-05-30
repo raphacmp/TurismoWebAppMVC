98a8624bc88cd39c4e4dd6434eaa63628d0228db
raphacmp <raphacmp@gmail.com>
2019-05-30 19:25:32 -0300

    Colaborador abstrato, Guia e Motorista iniciados

---
+++ TurismoWebAppMVC/Models/Guia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TurismoWebAppMVC.Models
+{
+    public class Guia : Colaborador
+    {
+        public string MatriculaNacional
+        {
+            get; set;
+        }
+
+        public Guia()
+        {
+        }
+
+        public Guia(int id, string nome, string cpf, string matriculaNacional) : base(id, nome, cpf)
+        {
+            MatriculaNacional = matriculaNacional;
+        }
+    }
+}
---
+++ TurismoWebAppMVC/Models/Motorista.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TurismoWebAppMVC.Models
+{
+    public class Motorista : Colaborador
+    {
+        public string Cnh
+        {
+            get; set;
+        }
+
+        public Motorista()
+        {
+        }
+
+        public Motorista(int id, string nome, string cpf, string cnh) : base(id, nome, cpf)
+        {
+            Cnh = cnh;
+        }
+    }
+}
